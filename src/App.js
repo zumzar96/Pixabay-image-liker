e7e838b8c970ddf08f3a3123c7130de19dbf8793
ismail <ismailgim96@gmail.com>
2022-03-31 14:26:02 +0200

    Adding homepage

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import  { useContext } from 'react';
 import { ImagesContext } from './context/images-context';
 import FavoriteItem from './components/FavoriteItem';
 import Main from './main'
+import Homepage from './components/homepage';
 
 
 
@@ -12,29 +13,12 @@ import Main from './main'
 function App() {
   
 
-  const Images = useContext(ImagesContext).images
-  const State = useContext(ImagesContext).state
+  
 
   return (
     <Fragment>
-      <div><FavoriteItem/></div>
-    <div className='max-w-sm rounded overflow-hidden my-10 mx-auto'>
-      <form>
-        <div className="flex items-center border-b border-b-2 border-teal-500 py-2">
-        <input onChange={e => State.setTerm(e.target.value)} className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Search Image Term..." />
-        <button className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="submit">
-      Search
-      </button>
-      </div>
-      </form>
-		</div>
-    <div className="container mx-auto">
-      <h1 className="text-6xl text-center mx-auto mt-32"></h1> : <div className="grid grid-cols-3 gap-4">
-        {Images.map(image => (
-          <ImageCard key={image.id} image={image} />
-        ))}
-      </div>
-    </div>
+      
+      <Homepage></Homepage>
     
     </Fragment>
   );
