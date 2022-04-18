5840a4fb45b732da6ce7a2884e7c09b9b252122d
ismail <ismailgim96@gmail.com>
2022-04-18 17:45:48 +0200

    bug fix

--- src/components/item.js
+++ src/components/item.js
@@ -69,6 +69,11 @@ const Item = (props) => {
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
             </svg>
           </div>
+          <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+            <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">Deactivate account</h3>
+            <div class="mt-2">
+              <p class="text-sm text-gray-500">Are you sure you want to deactivate your account? All of your data will be permanently removed. This action cannot be undone.</p>
+            </div>
           </div>
         </div>
       </div>
